ea89b63c8c06327dc1948f116fd39de6f342f63d
李小亮 <1396595541@qq.com>
2019-01-19 23:53:24 +0800

    权限列表展示完善，使用template模板加v-else进行字符串（0，1，2）和文字显示的转换

--- src/views/authority/authority.vue
+++ src/views/authority/authority.vue
@@ -28,6 +28,11 @@
             <el-table-column
                 prop="level"
                 label="层级">
+                <template slot-scope="scope">
+                   <span v-if=" scope.row.level === '0'">一级</span>
+                   <span v-else-if=" scope.row.level === '1' ">二级</span>
+                   <span v-else-if=" scope.row.level === '2' ">三级</span>
+                </template>
             </el-table-column>
       </el-table>
  </el-card>
