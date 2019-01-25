83d22bb9cb4c44dcc93796615a8c9efe288e9ee4
李小亮 <1396595541@qq.com>
2019-01-25 15:47:33 +0800

    数据加载时loading显示，ajax发送完毕后关闭

--- src/views/authority/roles.vue
+++ src/views/authority/roles.vue
@@ -1,16 +1,72 @@
 <template>
    <el-card class="card">
        <!-- 传值的父组件  子组件通过props接收 -->
-       <my-breadcrumb level1="权限管理" level2 = "权限列表"></my-breadcrumb>
+       <my-breadcrumb level1="权限管理" level2 = "角色列表"></my-breadcrumb>
+        <el-row :span="24">
+            <el-button type="success" class="roles" plain>添加角色</el-button>
+        </el-row>
+        <el-table
+        border
+        stripe
+        v-loading="loading"
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+       type="index"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="roleName"
+        label="角色名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="roleDesc"
+        label="角色描述"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+         <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" circle plain></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" plain circle></el-button>
+          <el-button type="success" icon="el-icon-check"  size="mini" plain circle></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
    </el-card>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            list:[],
+            loading:true
+        }
+    },
+    created(){
+     this.loadList();
+    },
+    methods:{
+     async loadList(){
+         let res = await this.$http.get('/roles');
+         let { meta: { msg, status } } = res.data;
+         this.loading = false;
+         if(status == 200){
+             this.list = res.data.data;
+         }else{
+             this.$message.error(msg); 
+         }
+     }
+    }
 }
 </script>
 <style>
 .card{
     height: 100%;
+}
+.roles{
+    margin-top: 10px;
+    margin-bottom: 10px;
 }
 </style>
