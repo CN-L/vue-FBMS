b76c6a50c9b0ba2e5f7c5000604db45c6d6333cb
李小亮 <1396595541@qq.com>
2019-01-28 17:32:20 +0800

    动态侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,66 +21,20 @@
       unique-opened
       class="menu"
       >
-      <el-submenu index="/users">
+      <el-submenu 
+      v-for = "item in list"
+      :key = "item.id"
+      :index="item.id">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span>用户管理</span>
+          <span>{{item.authName}}</span>
         </template>
-          <el-menu-item index="/users">
+          <el-menu-item
+          v-for = "item1 in item.children"
+          :key = "item1.id"
+          :index=" '/' + item1.path">
                 <i class="el-icon-menu"></i>
-                用户列表
-          </el-menu-item>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>权限管理</span>
-        </template>
-          <el-menu-item index="/roles">
-                <i class="el-icon-menu"></i>
-                角色列表
-          </el-menu-item>
-          <el-menu-item index="/rights">
-              <i class="el-icon-menu"></i>
-              权限列表
-          </el-menu-item>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>商品管理</span>
-        </template>
-          <el-menu-item index="3-1">
-                <i class="el-icon-menu"></i>
-               商品列表
-          </el-menu-item>
-          <el-menu-item index="3-2">
-              <i class="el-icon-menu"></i>
-              分类参数
-          </el-menu-item>
-          <el-menu-item index="3-3">
-              <i class="el-icon-menu"></i>
-              商品分类
-          </el-menu-item>
-      </el-submenu>
-      <el-submenu index="4">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>订单管理</span>
-        </template>
-          <el-menu-item index="4-1">
-                <i class="el-icon-menu"></i>
-                订单列表
-          </el-menu-item>
-      </el-submenu>
-            <el-submenu index="5">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>数据统计</span>
-        </template>
-          <el-menu-item index="4-1">
-                <i class="el-icon-menu"></i>
-                数据报表
+                {{item1.authName}}
           </el-menu-item>
       </el-submenu>
     </el-menu>
@@ -94,6 +48,11 @@
 </template>
 <script>
 export default {
+    data(){
+        return {
+             list:[]
+        }
+    },
     methods:{
         // 退出
       handleLogout(){
@@ -103,6 +62,10 @@ export default {
          this.$router.push('/login')
          //提示
          this.$message.success('退出成功')
+      },
+      async loadList(){
+        let res = await this.$http.get(`/menus`);
+        this.list = res.data.data;
       }
     },
     beforeCreate(){
@@ -115,6 +78,10 @@ export default {
             // 跳转登陆页面
             this.$router.push('/login')
      }
+    },
+    created () { 
+        // 动态菜单
+     this.loadList()
     }
 }
 </script>
