bd410380b5414c60bc01b9e29992868060f31402
李小亮 <1396595541@qq.com>
2019-02-08 15:54:10 +0800

    分类列表数据展示完成

--- src/views/goods/Goods.vue
+++ src/views/goods/Goods.vue
@@ -15,28 +15,36 @@
           <!-- table表格 -->
             <template>
                 <el-table
+                stripe
+                border
+                :data="list"
                 style="width: 100%">
                 <el-table-column
                     type="index"
-                    width="180">
+                    width="80">
                 </el-table-column>
                 <el-table-column
-                    prop=""
+                    prop="goods_name"
                     label="商品名称"
-                    width="180">
+                    width="400">
                 </el-table-column>
                 <el-table-column
-                    prop=""
+                    prop="goods_price"
                     label="商品价格（元）"
-                    width="180">
+                    width="120">
                 </el-table-column>
                 <el-table-column
-                    prop=""
-                    label="商品重量">
+                    prop="goods_weight"
+                    label="商品重量"
+                    width="150">
                 </el-table-column>
                 <el-table-column
-                    prop=""
                     label="创建时间">
+                    <template slot-scope="scope">
+                        <!-- 格式化日期 -->
+                        {{scope.row.add_time | formDate('YYYY-MM-DD')}}
+                    </template>
+                    
                 </el-table-column>
                 <el-table-column
                     label="操作">
@@ -55,7 +63,24 @@ export default {
     data(){
      return {
          searchValue:'',
+         list:[]
      }
+    },
+    methods:{
+        // 获取商品列表
+        async loadList(){
+          let res = await this.$http.get('goods?pagenum=1&pagesize=10');
+          let { meta: {msg, status}} = res.data;
+          if(status == 200){
+            //  请求成功
+            this.list = res.data.data.goods
+          }else {
+              this.$message.error(msg)
+          }
+        }
+    },
+    created(){
+        this.loadList();
     }
 }
 </script>
