76d0037a52c04d6641ae1ddf9b7314284d693e0d
李小亮 <1396595541@qq.com>
2019-03-03 19:24:59 +0800

    订单列表基本页面搭建 table表格和自己封装的组件

--- src/views/orders/Orders.vue
+++ src/views/orders/Orders.vue
@@ -1,13 +1,64 @@
 <template>
     <el-card class="card">
-      orders 
+      <!-- 面包系的组件 -->
+      <my-breadcrumb level1="订单管理" level2="订单列表"></my-breadcrumb>
+       <el-table
+      :data="tableData"
+      stripe
+      border
+      style="width: 100%,height:100%,margin-top: 10px">
+      <el-table-column
+      type="index"
+      width="60">
+      </el-table-column>
+      <el-table-column  
+        label="订单编号"
+        width="250">
+      </el-table-column>
+      <el-table-column
+        label="订单价格"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        label="是否付款"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop=""
+        label="是否发货"
+        width="100">
+      </el-table-column>
+       <el-table-column
+        prop=""
+        label="下单时间"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        label="操作">
+        <template slot-scope="scope">
+           <el-button
+           type="primary"
+           icon="el-icon-edit"
+           size="mini"
+           plain>
+           </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     </el-card>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            data:[]
+
+        }
+    }
 }  
 </script>
 <style>
- 
+ .el-table{
+     margin-top: 10px;
+ }
 </style>
