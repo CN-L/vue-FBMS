54edbc755d0884be4059aab6cac93d86546eed37
李小亮 <1396595541@qq.com>
2019-03-04 17:29:32 +0800

    百度echarts报表数据显示成功

--- src/views/reports/reports.vue
+++ src/views/reports/reports.vue
@@ -9,37 +9,41 @@
 // 导入echarts
 import echarts from 'echarts';
 export default {
-    data(){
-
-    },
     mounted(){
          this.init();
     },
     methods:{
-       init(){
-       // 初始化echarts
-       const  myChart = echarts.init(this.$refs.chart);
-       const option = {
-           title: {
-                text: 'ECharts 入门示例'
+       async init(){
+       let res = await this.$http.get(`reports/type/1`);
+       let option = res.data.data;
+       const data = {
+            title: {
+                text: '用户来源'
             },
-            tooltip: {},
-            legend: {
-                data:['销量']
+            tooltip : {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross',
+                    label: {
+                    backgroundColor: '#6a7985'
+                    }
+                }
             },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-       };
-        myChart.setOption(option);
-       }
-    }
+            // 用于保存为图片功能
+             toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+           },
+         }  
+           option = {...data, ...option}
+        //    理解为:横竖坐标轴 是否缩小  为true就会缩小
+           option.xAxis[0].boundaryGap = false; 
+            // 初始化echarts
+            const  myChart = echarts.init(this.$refs.chart);
+            myChart.setOption(option);
+            }
+        }
 }
 </script>
 
